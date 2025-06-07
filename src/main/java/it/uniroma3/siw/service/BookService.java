2f2154a8c38cdb5a7b077d83f9c58a8acb155b3f
mattiaderclub <mattialenti2003@gmail.com>
2025-06-07 08:27:08 +0200

    aggiunte classi repository e service per le altre classi model, poi sistemare associazioni e vincoli in modo tale da aggiungere getter, setter, hashCode e equals nei model

---
+++ src/main/java/it/uniroma3/siw/service/BookService.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.siw.repository.BookRepository;
+
+@Service
+public class BookService {
+
+	@Autowired
+	private BookRepository bookRepository;
+}
